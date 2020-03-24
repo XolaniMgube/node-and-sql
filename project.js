c0eb92f128321943cc9103b288ad7acbb1166f37
XolaniMgube <xnmgube@gmail.com>
2020-03-24 14:18:35 +0200

    Delete all function works

--- project.js
+++ project.js
@@ -1,30 +1,3 @@
-// npm install --save pg
-// find out more here: https://node-postgres.com/
-
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: "user",
-//   host: "localhost",
-//   database: "db",
-//   password: "pass",
-//   port: 5432
-// });
-
-// const helloWorld = () => {
-//   pool.query(
-//     "SELECT $1::text as message",
-//     ["Hello world!"],
-//     (error, results) => {
-//       if (error) {
-//         throw error;
-//       }
-
-//       console.log(results.rows);
-//     }
-//   );
-// };
-
-// helloWorld();
 
 const {Client} = require('pg')
 const client = new Client({
@@ -35,14 +8,8 @@ const client = new Client({
   database: "db"
 })
 
-// client.connect()
-// .then(() => console.log("Connected successfully"))
-// .then(() => client.query("select * from visitors"))
-// .then( results => console.table(results.rows))
-// .catch(e => console.log(e))
-// .finally(() => client.end())
 
-async function execute(){
+async function viewTable(){
   await client.connect()
   console.log("Connected successfully.")
   const results = await client.query("select * from visitors")
@@ -68,5 +35,25 @@ async function addVisitor(id, name, age, date, time, assistedBy, comments) {
   }
 }
 
+async function deleteAllVisitors() {
+  try{
+    await client.connect()
+    await client.query("BEGIN")
+    await client.query("delete from visitors")
+    console.log("Deleted all visitors")
+    await client.query("COMMIT")
+  }
+  catch(ex){
+    console.log("Failed to delete visitors" + ex)
+  }
+  finally{
+    await client.end()
+    console.log("script closed")
+  }
+}
+
+// addVisitor(1, 'Xolani', 25, '03-24-2020', '20:00', 'Kurtlin', 'He was drinking lean the whole time')
 // addVisitor(2, 'Lebo', 18, '03-24-2020', '20:00', 'Lentswe', 'I did not like Lentswe')
-execute()
+viewTable()
+// deleteAllVisitors()
+// viewTable()
