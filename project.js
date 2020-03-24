f089a630b483b1b011c5d48233a07f2153df32bf
XolaniMgube <xnmgube@gmail.com>
2020-03-24 13:12:03 +0200

    addVisitor with parameters function works

--- project.js
+++ project.js
@@ -35,9 +35,38 @@ const client = new Client({
   database: "db"
 })
 
-client.connect()
-.then(() => console.log("Connected successfully"))
-.then(() => client.query("select * from visitors"))
-.then( results => console.table(results.rows))
-.catch(e => console.log(e))
-.finally(() => client.end())
+// client.connect()
+// .then(() => console.log("Connected successfully"))
+// .then(() => client.query("select * from visitors"))
+// .then( results => console.table(results.rows))
+// .catch(e => console.log(e))
+// .finally(() => client.end())
+
+async function execute(){
+  await client.connect()
+  console.log("Connected successfully.")
+  const results = await client.query("select * from visitors")
+  console.table(results.rows)
+  await client.end()
+  console.log("Client disconnected successfully.")
+}
+
+async function addVisitor(id, name, age, date, time, assistedBy, comments) {
+  try{
+    await client.connect()
+    await client.query("BEGIN")
+    await client.query("insert into visitors values ($1, $2, $3, $4, $5, $6, $7)", [id, name, age, date, time, assistedBy, comments])
+    console.log("Inserted a new row")
+    await client.query("COMMIT")
+  }
+  catch(ex){
+    console.log("Failed to add visitor " + ex)
+  }
+  finally{
+    await client.end()
+    console.log("script closed")
+  }
+}
+
+// addVisitor(2, 'Lebo', 18, '03-24-2020', '20:00', 'Lentswe', 'I did not like Lentswe')
+execute()
